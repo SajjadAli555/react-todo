769883cf6d38a62fd425baa7959edb17ae5d0a3d
Sajjad Ali <sajjuali439@gmail.com>
2021-03-13 15:38:46 +0500

    new repository

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./App.css";
+import ToDo from "./component/ToDo";
+
+export default function App() {
+  return <ToDo />;
+}
---
+++ src/component/ToDoItem.js
@@ -0,0 +1,16 @@
+// import "./ToDoItem.css";
+
+const ToDoItem = (props) => {
+  const { item, deleteItem } = props;
+
+  return (
+    <div className="ToDoItem">
+      <p className="ToDoItem-Text">{item.text}</p>
+      <button className="ToDoItem-Delete" onClick={() => deleteItem(item.id)}>
+        -
+      </button>
+    </div>
+  );
+};
+
+export default ToDoItem;
